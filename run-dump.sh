379870b0c0a6f688eac481bb11ddf62c2d28f116
Steven Ward <planet36@users.noreply.github.com>
2026-01-07 17:23:58 -0500

    Add run-dump.sh

---
+++ run-dump.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/sh
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: OSL-3.0
+
+renice --priority 19 --pid $$ > /dev/null
+
+export LC_ALL=C
+
+TF=2
+TE=0
+TLMIN=32KB
+TLMAX=64KB
+
+OUTPUT_DIR=results
+DATETIME=$(date -u +'%Y%m%dT%H%M%S')
+
+mkdir --verbose --parents -- "$OUTPUT_DIR" || exit
+
+kill_jobs()
+{
+    # https://www.gnu.org/software/bash/manual/html_node/Job-Control-Builtins.html
+    # https://www.man7.org/linux/man-pages/man1/pkill.1.html
+
+    pkill --parent $$
+    sleep 0.5s
+    pkill --parent $$ --signal SIGTERM
+    sleep 0.5s
+    pkill --parent $$ --signal SIGKILL
+
+    exit
+}
+
+trap 'kill_jobs' INT
+
+for K in $(seq 1 2 15)
+do
+    echo "K=$K"
+
+    ./dump -f rdtsc  -k "$K"    | RNG_test stdin -tf $TF -te $TE -tlmin $TLMIN -tlmax $TLMAX -multithreaded > "${OUTPUT_DIR}/RNG_test.rdtsc.k-$K.pause-no.${DATETIME}.txt" &
+    ./dump -f rdtscp -k "$K"    | RNG_test stdin -tf $TF -te $TE -tlmin $TLMIN -tlmax $TLMAX -multithreaded > "${OUTPUT_DIR}/RNG_test.rdtscp.k-$K.pause-no.${DATETIME}.txt" &
+    ./dump -f rdtsc  -k "$K" -p | RNG_test stdin -tf $TF -te $TE -tlmin $TLMIN -tlmax $TLMAX -multithreaded > "${OUTPUT_DIR}/RNG_test.rdtsc.k-$K.pause-yes.${DATETIME}.txt" &
+    ./dump -f rdtscp -k "$K" -p | RNG_test stdin -tf $TF -te $TE -tlmin $TLMIN -tlmax $TLMAX -multithreaded > "${OUTPUT_DIR}/RNG_test.rdtscp.k-$K.pause-yes.${DATETIME}.txt" &
+
+    wait
+done
+
+echo
+echo "Result files which do NOT contain \"FAIL\":"
+
+command grep -L -F -w FAIL -- "${OUTPUT_DIR}/RNG_test."*".${DATETIME}.txt"
